70101e9a130d414c5262af41de8fbbc8bf8debea
benn.eichhorn <benn.eichhorn@e3d0c278-6bd3-11de-a9b1-77b8bb241e6b>
2009-07-08 19:25:24 +0000

    Changing directory structure

---
+++ wcf-pubsub-logger/LoggerLibrary/LoggerService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceModelEx;
+using LoggerLibrary.ServiceContracts;
+
+namespace LoggerLibrary
+{
+    class LoggerService : SubscriptionManager<ILogger>, ILoggerService
+    {
+        
+
+
+    }
+}
