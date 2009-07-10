9b908c40e3818c3507cff9ed23fe66d612a5ec84
benn.eichhorn <benn.eichhorn@e3d0c278-6bd3-11de-a9b1-77b8bb241e6b>
2009-07-10 09:39:21 +0000

    Created static LoggerClient handler

--- wcf-pubsub-logger/LoggerClientLibrary/Logger.cs
+++ wcf-pubsub-logger/LoggerClientLibrary/Logger.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿// © 2009 AlgoBox Ltd. All rights reserved
+// Benn Eichhorn
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using LoggerClientLibrary.LoggerPublishService;
 using System.Threading;
+using LoggerClientLibrary.Collections.Generics;
 
 namespace LoggerClientLibrary
 {
@@ -22,7 +26,8 @@ namespace LoggerClientLibrary
                     {
                         if (m_Logger == null)
                         {
-                            m_Logger = new Logger();                            
+                            m_Logger = new Logger();
+                            m_Logger.Open();
                         }
                     }
                 }
@@ -34,20 +39,23 @@ namespace LoggerClientLibrary
         LoggerClient m_LoggerClient = null;
 
         AutoResetEvent[] m_Events = null;
-
         AutoResetEvent m_EventExit = new AutoResetEvent(false);
-
         AutoResetEvent m_EventContinue = new AutoResetEvent(false);
 
+        const Int16 THREAD_CONTINUE = 0;
+        const Int16 THREAD_EXIT = 1;
+
         Thread m_Thread = null;
+
+        LockFreeQueue<Event> m_Queue = new LockFreeQueue<Event>();
 
         protected bool Open()
         {
             if(m_LoggerClient == null)
             {
                 m_Events = new AutoResetEvent[2];
-                m_Events[0] = m_EventContinue;
-                m_Events[1] = m_EventExit;
+                m_Events[THREAD_CONTINUE] = m_EventContinue;
+                m_Events[THREAD_EXIT] = m_EventExit;
 
                 try
                 {
@@ -55,6 +63,10 @@ namespace LoggerClientLibrary
                     {
                         m_LoggerClient = new LoggerClient();
 
+                        m_Thread = new Thread(new ThreadStart(ProcessThread));
+                        m_Thread.Name = "PubSubLoggerClient";
+                        m_Thread.Priority = ThreadPriority.BelowNormal;
+                        m_Thread.Start();
                     }
                     return true;
                 }
@@ -68,12 +80,65 @@ namespace LoggerClientLibrary
 
         protected bool Close()
         {
+            if (m_LoggerClient != null)
+            {
+                lock (m_LoggerClient)
+                {
+                    m_EventExit.Set();
+                    ProcessQueue();
+
+                    if (m_Thread != null)
+                    {
+                        m_Thread.Abort();
+                        m_Thread = null;
+                    }
+                }
+            }
             return false;
         }
 
-        protected void ProcessQueue()
+        protected void ProcessThread()
         {
+            bool isAlive = true;
+            int signalValue;
 
+            while (isAlive)
+            {
+                signalValue = WaitHandle.WaitAny(m_Events);
+                if (signalValue == THREAD_CONTINUE)
+                {
+                    ProcessQueue();
+                }
+                else if (signalValue == THREAD_EXIT)
+                {
+                    isAlive = false;
+                }
+            }
         }
+
+
+        private void ProcessQueue()
+        {
+            Event loggerEvent = null;
+            while (m_Queue.Deque(ref loggerEvent))
+            {
+                if (loggerEvent is LoggingEvent)
+                {
+                    m_LoggerClient.LoggingEvent((LoggingEvent)loggerEvent);
+                }
+                else if (loggerEvent is StatisticEvent)
+                {
+                    m_LoggerClient.StatisticEvent((StatisticEvent)loggerEvent);
+                } 
+                else if (loggerEvent is CounterEvent)
+                {
+                    m_LoggerClient.CounterEvent((CounterEvent)loggerEvent);
+                }
+            }
+        }
+
+        
+
+        public void Log(
     }
 }
